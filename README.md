dca597f192301342fce6847dc4870dbaf0481684
Austin Kang <austinkang14@gmail.com>
2019-08-16 14:36:11 -0700

    updated README.md

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # bamazon
+
+Bamazon is an app created using mySQL workbench and node. When the application is ran, the customer will have a choice of which items they are able to purchase based on the database/table created in mySQL. It will show the item_id, item_name, department_name, price and stock_quantity. The user will be able to choose the amount they want and it should show the total added in the console. If the user inputs a number greater than the stock_quantity available it will return a message saying "oops sorry!".
+
+Here is the link to the working app: 
+https://drive.google.com/file/d/1Nhbk9E3dlNHOIi9UJgXavZ8lsMr03QVJ/view
--- bamazon.js
+++ bamazon.js
@@ -3,80 +3,80 @@ var inquirer = require("inquirer");
 
 var connection = mysql.createConnection({
 
-host:"localhost",
-port: 3306,
-user:"root",
-password:"8652812aK14",
-database:"bamazon"
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "8652812aK14",
+    database: "bamazon"
 
 
 });
 
 products();
 
-function products(){
+function products() {
 
 
-connection.connect(function(err) {
-    if (err) throw err;
-    console.log("Connected!");
-  });
+    connection.connect(function (err) {
+        if (err) throw err;
+        console.log("Connected!");
+    });
 
 
 
-connection.query("SELECT * FROM products", function(err,results){
-    if (err) throw err;
-    for (var i = 0;  i < results.length; i++){
-        console.log("Product ID: " + results[i].item_id);
-        console.log("Product: " + results[i].product_name);
-        console.log("Price:" + "$" + results[i].price);
-        console.log("--------------------------------");
-    }
+    connection.query("SELECT * FROM products", function (err, results) {
+        if (err) throw err;
+        for (var i = 0; i < results.length; i++) {
+            console.log("Product ID: " + results[i].item_id);
+            console.log("Product: " + results[i].product_name);
+            console.log("Price:" + "$" + results[i].price);
+            console.log("--------------------------------");
+        }
 
-    inquirer.prompt([
-        {
-            name:"items",
-            type:"rawlist",
-            message:"Which item_id would you like to buy?",
-            choices: function(){
-                choiceArr = [];
-                for (var i = 0; i < results.length; i++){
-                    choiceArr.push(results[i].item_id);
-                }
-    
-                return choiceArr;
+        inquirer.prompt([
+            {
+                name: "items",
+                type: "rawlist",
+                message: "Which item_id would you like to buy?",
+                choices: function () {
+                    choiceArr = [];
+                    for (var i = 0; i < results.length; i++) {
+                        choiceArr.push(results[i].item_id);
+                    }
+
+                    return choiceArr;
+
+                },
+
 
             },
-            
-
-        },
-        {
-            name:"Amount",
-            type:"input",
-            message:"How many would you like to buy?"
-        }
-    ]).then(function(answer){
-        var itemChoice = answer.items;
-        var amountChoice = answer.Amount;
-        
-        connection.query("SELECT stock_quantity FROM products WHERE item_id = " + itemChoice, function(err,results){
-            if (err) throw err;
-            for (var i = 0; i < results.length; i++){
-                if (results[i].stock_quantity >= amountChoice){
-                    console.log("Here is your total!");
-                    var newQuantity = results[i].stock_quantity - amountChoice;
-                    connection.query('UPDATE products SET stock_quantity = ' + newQuantity + ' WHERE item_id = ' + itemChoice);
-                    connection.query("SELECT price FROM products WHERE item_id = " + itemChoice, function(err,results){
-                        console.log("Your total is: $ "  + results[0].price * amountChoice);
-                        connection.end();
-                    })
-                } else {
-                    console.log("Oops, you can't do that sorry!");
-                    connection.end();
-                }
+            {
+                name: "Amount",
+                type: "input",
+                message: "How many would you like to buy?"
             }
-        })
-    })
+        ]).then(function (answer) {
+            var itemChoice = answer.items;
+            var amountChoice = answer.Amount;
 
-});
+            connection.query("SELECT stock_quantity FROM products WHERE item_id = " + itemChoice, function (err, results) {
+                if (err) throw err;
+                for (var i = 0; i < results.length; i++) {
+                    if (results[i].stock_quantity >= amountChoice) {
+                        console.log("Here is your total!");
+                        var newQuantity = results[i].stock_quantity - amountChoice;
+                        connection.query('UPDATE products SET stock_quantity = ' + newQuantity + ' WHERE item_id = ' + itemChoice);
+                        connection.query("SELECT price FROM products WHERE item_id = " + itemChoice, function (err, results) {
+                            console.log("Your total is: $ " + results[0].price * amountChoice);
+                            connection.end();
+                        })
+                    } else {
+                        console.log("Oops, you can't do that sorry!");
+                        connection.end();
+                    }
+                }
+            })
+        })
+
+    });
 };
